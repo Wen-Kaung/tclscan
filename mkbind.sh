03bcc7e6719eec56faf74462aad79fa55f87c200
Wen-Kaung <55080359+Wen-Kaung@users.noreply.github.com>
2019-09-09 15:50:07 +0800

    Update mkbind.sh

--- mkbind.sh
+++ mkbind.sh
@@ -1,3 +1,1 @@
-cd ~/rust/rust-bindgen/target/debug
-LD_PRELOAD=/usr/lib/llvm-3.4/lib/libclang.so ./bindgen -ltcl -builtins \
-    -o $(cd -)/src/tcl.rs $(cd -)/src/mytcl.h
+when HTTP_REQUEST { HTTP::header insert "client" [IP::client_addr] }
